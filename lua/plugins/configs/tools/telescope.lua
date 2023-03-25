dd09e4b9e67fbbe332a8c52fd2cc3588791caa40
Emmanuel Bustos <ema2159@gmail.com>
2023-03-25 15:03:57 +0200

    mod: Update telescope config

--- lua/plugins/configs/tools/telescope.lua
+++ lua/plugins/configs/tools/telescope.lua
@@ -6,14 +6,34 @@ return function()
         preview_title = false,
         results_title = false,
         theme = "ivy",
+        layout_config = {
+          height = 15
+        },
+      },
+      commands = {
+        theme = "dropdown",
+        layout_config = {
+          anchor = "N"
+        }
+      },
+      colorscheme = {
+        enable_preview = true,
+        theme = "dropdown",
+        layout_config = {
+          anchor = "N"
+        }
       }
     }
   })
 
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
   vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
   vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>hm', builtin.keymaps, {})
+  vim.keymap.set('n', '<leader>ot', builtin.colorscheme, {})
+  vim.keymap.set('n', '<M-x>', builtin.commands, {})
   vim.keymap.set('n', '/', builtin.current_buffer_fuzzy_find, {})
 end
 
