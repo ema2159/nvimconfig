8dbe9481c3341a582e251d7f861e1e15b50c997e
Emmanuel Bustos <ema2159@gmail.com>
2023-03-26 20:57:10 +0300

    feat: Add Onedark theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -8,6 +8,10 @@ themes = {
     "marko-cerovac/material.nvim",
     config = require("themes.material"),
   },
+  -- One Dark
+  {
+    "joshdick/onedark.vim",
+  },
 }
 
 -- vim.g.material_style = "oceanic"
