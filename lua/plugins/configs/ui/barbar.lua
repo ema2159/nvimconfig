4431bfa7e4f96a4f9180447980370b3480c8a6a8
Emmanuel Bustos <ema2159@gmail.com>
2023-03-26 18:31:28 +0300

    feat: Expand barbar keymaps

--- lua/plugins/configs/ui/barbar.lua
+++ lua/plugins/configs/ui/barbar.lua
@@ -2,7 +2,13 @@ return function()
   -- Move to previous/next
   vim.keymap.set("n", "<C-PageUp>", "<Cmd>BufferPrevious<CR>", {})
   vim.keymap.set("n", "<C-PageDown>", "<Cmd>BufferNext<CR>", {})
+  vim.keymap.set("i", "<C-PageUp>", "<Cmd>BufferPrevious<CR>", {})
+  vim.keymap.set("i", "<C-PageDown>", "<Cmd>BufferNext<CR>", {})
   -- Re-order tabs
   vim.keymap.set("n", "<C-S-PageUp>", "<Cmd>BufferMovePrevious<CR>", {})
   vim.keymap.set("n", "<C-S-PageDown>", "<Cmd>BufferMoveNext<CR>", {})
+  vim.keymap.set("i", "<C-S-PageUp>", "<Cmd>BufferMovePrevious<CR>", {})
+  vim.keymap.set("i", "<C-S-PageDown>", "<Cmd>BufferMoveNext<CR>", {})
+  -- Buffer-picking mode
+  vim.keymap.set("n", "<leader>ta", "<Cmd>BufferPick<CR>", {})
 end
