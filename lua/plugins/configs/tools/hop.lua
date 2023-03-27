260b746bf959ada905ce35e8fd68856a6e226f40
Emmanuel Bustos <ema2159@gmail.com>
2023-03-27 22:59:42 +0300

    refactor: Remove dead code

--- lua/plugins/configs/tools/hop.lua
+++ lua/plugins/configs/tools/hop.lua
@@ -1,7 +1,6 @@
 return function()
   local hop = require("hop")
   hop.setup()
-  local directions = require("hop.hint").HintDirection
   vim.keymap.set("", "<leader>'", function()
     hop.hint_char1()
   end)
