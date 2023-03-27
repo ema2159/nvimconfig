e6ceace980676f1910d869d1751f8f4192da28c6
Emmanuel Bustos <ema2159@gmail.com>
2023-03-27 19:50:48 +0300

    mod: Update wilder configuration

--- lua/plugins/configs/ui/wilder.lua
+++ lua/plugins/configs/ui/wilder.lua
@@ -1,10 +1,11 @@
 return function()
   local wilder = require("wilder")
-  wilder.setup({modes = {":", "/", "?"}})
+  wilder.setup({modes = {":"}})
   wilder.set_option("renderer", wilder.popupmenu_renderer(
   wilder.popupmenu_palette_theme({
     -- "single", "double", "rounded" or "solid"
     -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+    highlighter = wilder.basic_highlighter(),
     border = "rounded",
     max_height = "75%",      -- max height of the palette
     min_height = 0,          -- set to the same as "max_height" for a fixed height window
