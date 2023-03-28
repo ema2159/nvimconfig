b00857ec3523992385fa86cdfa5bb35960d46546
Emmanuel Bustos <ema2159@gmail.com>
2023-03-28 19:44:45 +0300

    mod: Update toggleterm config

--- lua/plugins/configs/tools/toggleterm.lua
+++ lua/plugins/configs/tools/toggleterm.lua
@@ -1,19 +1,25 @@
-return function()
-  require("toggleterm").setup()
-
+local function config_lazygit()
   local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, dir = "git_dir", direction = "float", size=70 })
+  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, dir = "git_dir", direction = "float", size = 70 })
 
   local function lazygit_toggle()
     lazygit:toggle()
   end
 
   vim.keymap.set("n", "<leader>gs", lazygit_toggle, { desc = "LazyGit status" })
+end
+
+return function()
+  require("toggleterm").setup()
 
   vim.api.nvim_set_keymap(
     "n",
     "<F5>",
-    "<Cmd>ToggleTerm size=20 dir=~/Desktop direction=horizontal<CR>",
+    "<Cmd>ToggleTerm size=20 dir=%d direction=horizontal<CR>",
     { desc = "Toggle terminal" }
   )
+  vim.keymap.set("t", "<F5>", "<Cmd>ToggleTerm<CR>", opts)
+
+  -- Lazygit
+  config_lazygit()
 end
