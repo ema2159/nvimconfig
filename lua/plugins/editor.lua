038e1aa8487616ca11622546ffc2c31dc51bd7c6
Emmanuel Bustos <ema2159@gmail.com>
2023-03-30 17:58:01 +0300

    refactor: Divide config extracting editor plugins from tools

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,27 @@
+editor = {
+  -- Comment
+  {
+    "numToStr/Comment.nvim",
+    config = require("editor.comment"),
+  },
+  -- Hop
+  {
+    "phaazon/hop.nvim",
+    config = require("editor.hop"),
+  },
+  -- cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "onsails/lspkind.nvim",
+    },
+    event = "InsertEnter",
+    config = require("editor.cmp"),
+  },
+}
+
+return editor
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,9 @@
 local themes = require("plugins.themes")
 local tools = require("plugins.tools")
 local ui = require("plugins.ui")
+local editor = require("plugins.editor")
 
-plugins = { themes, ui, tools }
+plugins = { themes, ui, tools, editor }
 
 require("lazy").setup(plugins, opts)
 
