bc3851aeb1ef5bfc65d49ad8c74112c3b6928bd4
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-03-30 07:29:50 +0300

    Set GUI font options for Neovide

--- lua/plugins/configs/tools/comment.lua
+++ lua/plugins/configs/tools/comment.lua
@@ -2,10 +2,10 @@ return function()
   require("Comment").setup({
     sticky = false,
     toggler = {
-      line = "<C-_>", -- C-7
+      line = "<C-c>", -- C-7
     },
     opleader = {
-      line = "<C-_>", -- C-7
+      line = "<C-c>", -- C-7
     },
     post_hook = function()
       vim.cmd("norm! j")
