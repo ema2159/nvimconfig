9f229f930c58bedcc24b1cb25d69ad93c02b272e
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-03-30 11:50:43 +0300

    mod: Disable noice.nvim with Neovide

--- lua/plugins/configs/ui/noice.lua
+++ lua/plugins/configs/ui/noice.lua
@@ -5,23 +5,25 @@ local presets = {
 }
 
 return function()
-  require("noice").setup({
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
+  if not vim.g.neovide then
+    require("noice").setup({
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
       },
-    },
-    presets = presets,
-    messages = {
-      -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-      -- This is a current Neovim limitation.
-      enabled = false, -- enables the Noice messages UI
-      view = false, -- default view for messages
-      view_error = "notify", -- view for errors
-      view_warn = "notify", -- view for warnings
-      view_history = "messages", -- view for :messages
-      view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-    },
-  })
+      presets = presets,
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = false, -- enables the Noice messages UI
+        view = false, -- default view for messages
+        view_error = "notify", -- view for errors
+        view_warn = "notify", -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
+    })
+  end
 end
