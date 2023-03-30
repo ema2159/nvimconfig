52fa93a200a9d97ac4e63113d92de128ccd593ea
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-03-30 07:51:08 +0300

    Revert "Set GUI font options for Neovide"
    This reverts commit bc3851aeb1ef5bfc65d49ad8c74112c3b6928bd4.

--- lua/plugins/configs/tools/comment.lua
+++ lua/plugins/configs/tools/comment.lua
@@ -2,10 +2,10 @@ return function()
   require("Comment").setup({
     sticky = false,
     toggler = {
-      line = "<C-c>", -- C-7
+      line = "<C-_>", -- C-7
     },
     opleader = {
-      line = "<C-c>", -- C-7
+      line = "<C-_>", -- C-7
     },
     post_hook = function()
       vim.cmd("norm! j")
