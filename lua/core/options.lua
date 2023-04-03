a829b0a74ffd4c89529bfb93da61e341a41e7279
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-04-03 17:31:49 +0300

    feat: Use powershell as default shell in windows

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -34,3 +34,11 @@ vim.opt.termguicolors = true
 
 -- Set GUI font
 vim.opt.guifont = { "JetBrainsMono NFM", ":h12" }
+
+-- Set default shell for Windows as Powershell
+if vim.fn.has("windows") then
+  vim.opt.shell = "powershell.exe"
+  vim.opt.shellcmdflag = "-command"
+  vim.opt.shellquote = "\""
+  vim.opt.shellxquote = ""
+end
