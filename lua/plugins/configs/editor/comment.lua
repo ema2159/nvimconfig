5d3d6d3a8b2bc18c213835dc169fb8b7ecf07b0c
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-04-03 18:10:16 +0300

    mod: Change block comment keybindings

--- lua/plugins/configs/editor/comment.lua
+++ lua/plugins/configs/editor/comment.lua
@@ -3,9 +3,11 @@ return function()
     sticky = false,
     toggler = {
       line = "<C-_>", -- C-7
+      block = "<leader>c",
     },
     opleader = {
       line = "<C-_>", -- C-7
+      block = "<leader>c",
     },
     post_hook = function()
       vim.cmd("norm! j")
