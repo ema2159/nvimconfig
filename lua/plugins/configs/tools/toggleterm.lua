48f8ef175ba6f8632ad5406bb7560e6482d45a0a
Emmanuel Bustos <ema2159@gmail.com>
2023-04-06 18:18:58 +0300

    feat: Update lazygit configuration

--- lua/plugins/configs/tools/toggleterm.lua
+++ lua/plugins/configs/tools/toggleterm.lua
@@ -1,8 +1,26 @@
 local function config_lazygit()
   local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, dir = "git_dir", direction = "float", size = 70 })
+  local lazygit = Terminal:new({
+    cmd = "lazygit",
+    direction = "float",
+    dir = "git_dir",
+    size = 70,
+    float_opts = {
+      border = "double",
+    },
+    close_on_exit = true,
+    -- function to run on opening the terminal
+    on_open = function(_term)
+      vim.cmd("startinsert!")
+    end,
+    -- function to run on closing the terminal
+    on_close = function(_term)
+      vim.cmd("startinsert!")
+    end,
+  })
 
   local function lazygit_toggle()
+    lazygit.dir = vim.fn.expand("%:p:h")
     lazygit:toggle()
   end
 
