92b894ecab2a9248afb513bdefd34ae5a35c9c9c
Emmanuel Bustos <ema2159@gmail.com>
2023-04-06 17:31:09 +0300

    feat: Use default Rust analyzer configuration

--- lua/plugins/configs/tools/lspconfig.lua
+++ lua/plugins/configs/tools/lspconfig.lua
@@ -24,12 +24,7 @@ local function servers_config(lspconfig)
   })
   lspconfig.pyright.setup({})
   lspconfig.tsserver.setup({})
-  lspconfig.rust_analyzer.setup({
-    -- Server-specific settings. See `:help lspconfig-setup`
-    settings = {
-      ["rust-analyzer"] = {},
-    },
-  })
+  lspconfig.rust_analyzer.setup({})
 end
 
 local function keymaps_config()
