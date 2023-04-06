ae65042a91c2c94f1c0d227f0dc95517974a3849
Emmanuel Bustos <emmanuel.bustos@dispelix.com>
2023-04-06 09:29:34 +0300

    feat: Add Rosé Pine

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,69 +1,73 @@
 local themes = {
-  -- Catppuccin
-  {
-    "catppuccin/nvim",
-  },
-  -- Material
-  {
-    "marko-cerovac/material.nvim",
-    config = require("themes.material"),
-  },
-  -- One Dark
-  {
-    "olimorris/onedarkpro.nvim",
-  },
-  -- Monokai Pro
-  {
-    "loctvl842/monokai-pro.nvim",
-    config = require("themes.monokai_pro"),
-  },
-  -- Dracula
-  {
-    "Mofiqul/dracula.nvim",
-  },
-  -- Kanagawa
-  {
-    "rebelot/kanagawa.nvim",
-  },
-  -- Gruvbox Material
-  {
-    "ellisonleao/gruvbox.nvim",
-  },
-  -- VSCode (Dark+ and Light+)
-  {
-    "Mofiqul/vscode.nvim",
-  },
-  -- Melange
-  {
-    "savq/melange-nvim",
-  },
-  -- Nord
-  {
-    "shaunsingh/nord.nvim",
-  },
-  -- Nightfly
-  {
-    "bluz71/vim-nightfly-colors",
-  },
-  -- Moonfly
-  {
-    "bluz71/vim-moonfly-colors",
-  },
-  -- Tokyonight
-  {
-    "folke/tokyonight.nvim",
-  },
-  -- Oxacarbon
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-  },
-  -- GitHub Nvim Theme
-  {
-    "projekt0n/github-nvim-theme",
-  },
-  -- Nightfox
-  {
-    "EdenEast/nightfox.nvim",
+	-- Catppuccin
+	{
+		"catppuccin/nvim",
+	},
+	-- Material
+	{
+		"marko-cerovac/material.nvim",
+		config = require("themes.material"),
+	},
+	-- One Dark
+	{
+		"olimorris/onedarkpro.nvim",
+	},
+	-- Monokai Pro
+	{
+		"loctvl842/monokai-pro.nvim",
+		config = require("themes.monokai_pro"),
+	},
+	-- Dracula
+	{
+		"Mofiqul/dracula.nvim",
+	},
+	-- Kanagawa
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	-- Gruvbox Material
+	{
+		"ellisonleao/gruvbox.nvim",
+	},
+	-- VSCode (Dark+ and Light+)
+	{
+		"Mofiqul/vscode.nvim",
+	},
+	-- Melange
+	{
+		"savq/melange-nvim",
+	},
+	-- Nord
+	{
+		"shaunsingh/nord.nvim",
+	},
+	-- Nightfly
+	{
+		"bluz71/vim-nightfly-colors",
+	},
+	-- Moonfly
+	{
+		"bluz71/vim-moonfly-colors",
+	},
+	-- Tokyonight
+	{
+		"folke/tokyonight.nvim",
+	},
+	-- Oxacarbon
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+	},
+	-- GitHub Nvim Theme
+	{
+		"projekt0n/github-nvim-theme",
+	},
+	-- Nightfox
+	{
+		"EdenEast/nightfox.nvim",
+	},
+	-- Rosé Pine
+	{ 
+    "rose-pine/neovim"
   },
 }
 
