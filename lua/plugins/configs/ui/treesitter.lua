fcee72551997168fc8ab6e878da597089ea9b21a
Emmanuel Bustos <ema2159@gmail.com>
2023-04-27 09:09:50 +0300

    mod: Install markdown and markdown_inline by default for lspsaga

--- lua/plugins/configs/ui/treesitter.lua
+++ lua/plugins/configs/ui/treesitter.lua
@@ -1,7 +1,7 @@
 return function()
   require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -14,7 +14,7 @@ return function()
       enable = true,
 
       -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-      disable = function(lang, buf)
+      disable = function(_lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
