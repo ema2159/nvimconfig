4877247b07cc78dfac45f2584b5fcc30de7ef09a
Emmanuel Bustos <ema2159@gmail.com>
2023-04-27 09:00:52 +0300

    fix: Warning for non-existing help tree sitter parser

--- lua/plugins/configs/ui/treesitter.lua
+++ lua/plugins/configs/ui/treesitter.lua
@@ -1,7 +1,7 @@
 return function()
   require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "help", "query" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
