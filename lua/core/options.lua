71e14e5cff3d2fcdd1765467eda106791de20210
Emmanuel Bustos <ema2159@gmail.com>
2023-05-09 14:03:36 +0300

    feat: Update Windows powershell configuration

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -36,8 +36,16 @@ vim.opt.guifont = { "JetBrainsMono NFM", ":h12" }
 
 -- Set default shell for Windows as Powershell
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.opt.shell = "powershell.exe"
-  vim.opt.shellcmdflag = "-command"
-  vim.opt.shellquote = '"'
-  vim.opt.shellxquote = ""
+  local powershell_options = {
+    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 end
