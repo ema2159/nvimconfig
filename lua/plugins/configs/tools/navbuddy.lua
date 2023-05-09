1d48d0852b63b613adab5809bfe4857b5b12e45d
Emmanuel Bustos <ema2159@gmail.com>
2023-05-09 21:05:56 +0300

    fix: Update navbuddy

--- lua/plugins/configs/tools/navbuddy.lua
+++ lua/plugins/configs/tools/navbuddy.lua
@@ -7,10 +7,10 @@ return function()
       border = "rounded", -- "rounded", "double", "solid", "none"
     },
     mappings = {
-      ["<S-Tab>"] = actions.parent, -- Move to left panel
-      ["<Tab>"] = actions.children, -- Move to right panel
-      ["<Left>"] = actions.parent, -- Move to left panel
-      ["<Right>"] = actions.children, -- Move to right panel
+      ["<S-Tab>"] = actions.parent(), -- Move to left panel
+      ["<Tab>"] = actions.children(), -- Move to right panel
+      ["<Left>"] = actions.parent(), -- Move to left panel
+      ["<Right>"] = actions.children(), -- Move to right panel
     },
     lsp = { auto_attach = true },
   })
