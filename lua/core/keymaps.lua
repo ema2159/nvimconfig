07486874e9096bf0c1e711364034f5f10153255b
Emmanuel Bustos <ema2159@gmail.com>
2023-05-12 12:50:06 +0300

    feat: Enter normal mode in terminal when pressing ^<Esc^>

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>bk", "<Cmd>bd<CR>", { desc = "Close buffer" })
 vim.keymap.set("i", "<Tab>", "<C-f>", { desc = "Place cursor into indent position" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
