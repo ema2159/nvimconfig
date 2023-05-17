28e5249c43d80f3ae1513ba646529f6e91ea343b
Emmanuel Bustos <ema2159@gmail.com>
2023-05-17 17:52:18 +0300

    feat: Disable lualine in NvimTree

--- lua/plugins/configs/ui/lualine.lua
+++ lua/plugins/configs/ui/lualine.lua
@@ -1,5 +1,8 @@
 return function()
   require("lualine").setup({
+    options = {
+      disabled_filetypes = { "NvimTree" },
+    },
     sections = {
       lualine_x = { "encoding", "filesize", "fileformat", "filetype" },
     },
