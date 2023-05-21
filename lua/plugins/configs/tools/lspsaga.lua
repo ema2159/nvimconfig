2389c0d841ea0e66921b3c7eb8b5ff5d699818cc
Emmanuel Bustos <ema2159@gmail.com>
2023-05-21 09:51:35 +0300

    doc: Improve keybinding doc

--- lua/plugins/configs/tools/lspsaga.lua
+++ lua/plugins/configs/tools/lspsaga.lua
@@ -52,7 +52,7 @@ return function()
   vim.keymap.set("n", "<leader>lD", "<Cmd>Lspsaga peek_type_definition<CR>", { desc = "Type definition" })
   vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", { desc = "Hover" })
   vim.keymap.set("n", "<leader>lo", "<Cmd>Lspsaga outline<CR>", { desc = "Show outline" })
-  vim.keymap.set("n", "<leader>lr", "<Cmd>Lspsaga rename ++project<CR>", { desc = "Rename" })
+  vim.keymap.set("n", "<leader>lr", "<Cmd>Lspsaga rename ++project<CR>", { desc = "Rename in project" })
   vim.keymap.set({ "n", "v" }, "<leader>la", "<Cmd>Lspsaga code_action<CR>", { desc = "Code actions" })
   vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { desc = "LSP finder" })
 end
