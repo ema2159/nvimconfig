9d6eb16b630a4460974a3fa7b5547ce5a37288f9
Emmanuel Bustos <ema2159@gmail.com>
2023-11-06 18:05:17 +0100

    Add autocommand to vim illuminate to default to Visual highlight when changing colorschemes

--- lua/plugins/configs/ui/vim_illuminate.lua
+++ lua/plugins/configs/ui/vim_illuminate.lua
@@ -6,7 +6,12 @@ return function()
     },
     modes_allowlist = { "n", "i" },
   })
-  vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-  vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-  vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function()
+      vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+      vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+      vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+    end,
+  })
 end
