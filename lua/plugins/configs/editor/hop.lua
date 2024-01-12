f30fb71a86274e175d71676b9eb143d82f99f7c2
Emmanuel Bustos Torres <ema2159@gmail.com>
2024-01-12 10:33:27 +0100

    Enable hop on visual mode

--- lua/plugins/configs/editor/hop.lua
+++ lua/plugins/configs/editor/hop.lua
@@ -1,7 +1,7 @@
 return function()
   local hop = require("hop")
   hop.setup()
-  vim.keymap.set("n", "<leader>'", function()
+  vim.keymap.set({"n", "v"}, "<leader>'", function()
     hop.hint_char1()
   end, { desc = "Hop to character" })
 end
