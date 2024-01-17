dfbec4eca73a499cee56bafa95810df69a2f83e7
Emmanuel Bustos Torres <ema2159@gmail.com>
2024-01-17 15:00:57 +0100

    Add keymap to toggle diagnostics

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,5 @@
+local Utils = require("core.utils")
+
 vim.keymap.set("n", "<leader>bk", "<Cmd>bd<CR>", { desc = "Close buffer" })
 vim.keymap.set("i", "<Tab>", "<C-f>", { desc = "Place cursor into indent position" })
+vim.keymap.set("n", "<leader>ld", function() Utils.toggle_diagnostics() end, { desc = "Toggle diagnostincs" })
