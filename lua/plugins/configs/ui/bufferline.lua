3cad17257ce43bc4c0bd778a289cd4d3f3979f1d
Emmanuel Bustos <ema2159@gmail.com>
2024-04-13 09:47:08 +0200

    feat: Try bufferline groups

--- lua/plugins/configs/ui/bufferline.lua
+++ lua/plugins/configs/ui/bufferline.lua
@@ -28,6 +28,30 @@ return function()
           reveal = { "close" },
         },
       },
+      groups = {
+        options = {
+          toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+        },
+        items = {
+          {
+            name = "Test", -- Mandatory
+            priority = 2, -- determines where it will appear relative to other groups (Optional)
+            matcher = function(buf) -- Mandatory
+              return buf.name:match("%_test") or buf.name:match("%_spec")
+            end,
+          },
+          {
+            name = "Docs",
+            auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+            matcher = function(buf)
+              return buf.name:match("%.md") or buf.name:match("%.txt")
+            end,
+            separator = { -- Optional
+              style = require("bufferline.groups").separator.tab,
+            },
+          },
+        },
+      },
     },
   })
 
