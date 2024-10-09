43e25c5c36cdc8ee5f14ab54f2cc9f9351c56d52
Emmanuel Bustos Torres <ema2159@gmail.com>
2024-10-09 08:49:51 +0200

    Update which_key.lua

--- lua/plugins/configs/ui/which_key.lua
+++ lua/plugins/configs/ui/which_key.lua
@@ -2,8 +2,6 @@ return function()
   vim.o.timeout = true
   vim.o.timeoutlen = 300
   require("which-key").setup({
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+      preset = "helix",
   })
 end
