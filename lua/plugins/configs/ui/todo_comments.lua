f5fc43a7206daeccc71a1ca06a7259e7a64f3757
Emmanuel Bustos Torres <ema2159@gmail.com>
2024-10-21 08:28:13 +0200

    Add: Todo trouble keybindings

--- lua/plugins/configs/ui/todo_comments.lua
+++ lua/plugins/configs/ui/todo_comments.lua
@@ -1,3 +1,17 @@
 return function()
   require("todo-comments").setup()
+
+    vim.keymap.set(
+    "n",
+    "<leader>xd",
+    "<cmd>Trouble todo focus=true filter.buf=0<cr>",
+    { desc = "Todo comments current buffer (Trouble)", silent = true, noremap = true }
+  )
+
+  vim.keymap.set(
+    "n",
+    "<leader>xD",
+    "<cmd>Trouble todo focus=true<cr>",
+    { desc = "Todo comments all (Trouble)", silent = true, noremap = true }
+  )
 end
