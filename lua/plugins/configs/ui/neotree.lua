de7651e59f2bb2753f72f8f4e1c0295251eb192d
Emmanuel Bustos <ema2159@gmail.com>
2024-10-24 20:47:13 +0200

    feat: Bind default Vim search to / in neo-tree

--- lua/plugins/configs/ui/neotree.lua
+++ lua/plugins/configs/ui/neotree.lua
@@ -3,6 +3,16 @@ return function()
     popup_border_style = "solid",
     window = {
       position = "float",
+      mappings = {
+        -- Enable default Vim searching
+        ["/"] = {
+          function()
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("/", true, false, true), "n", false)
+          end,
+          desc = "Find in Neotree buffer",
+        },
+        ["<C-s>"] = "fuzzy_finder",
+      },
     },
   })
   vim.keymap.set("n", "<leader>ff", "<Cmd>Neotree reveal<CR>", { desc = "Open tree browser" })
