918a5d9da1773384ef7d33539c6546d723615180
Emmanuel Bustos <ema2159@gmail.com>
2024-10-26 11:54:17 +0200

    feat: Add copy name and copy path to clipboard feature to neo-tree

--- lua/plugins/configs/ui/neotree.lua
+++ lua/plugins/configs/ui/neotree.lua
@@ -1,3 +1,23 @@
+local function copy_to_reg(content)
+  local clipboard_name = "system"
+  local reg = "+"
+
+  -- manually firing TextYankPost does not set vim.v.event
+  -- workaround: create a scratch buffer with the clipboard contents and send a yank command
+  local temp_buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_text(temp_buf, 0, 0, 0, 0, { content })
+  vim.api.nvim_buf_call(temp_buf, function()
+    vim.cmd(string.format('normal! "%sy$', reg))
+  end)
+  vim.api.nvim_buf_delete(temp_buf, {})
+
+  vim.notify(
+    string.format("Copied %s to %s clipboard!", content, clipboard_name),
+    vim.log.levels.INFO,
+    { title = "NvimTree" }
+  )
+end
+
 return function()
   require("neo-tree").setup({
     popup_border_style = "solid",
@@ -12,6 +32,22 @@ return function()
           desc = "Find in Neotree buffer",
         },
         ["<C-s>"] = "fuzzy_finder",
+        ["y"] = {
+          function(state)
+            local name = state.tree:get_node().name
+            copy_to_reg(name)
+          end,
+          desc = "Copy name to clipboard",
+          nowait = true,
+        },
+        ["gy"] = {
+          function(state)
+            local path = state.tree:get_node().path
+            copy_to_reg(path)
+          end,
+          desc = "Copy path to clipboard",
+          nowait = true,
+        },
       },
     },
   })
