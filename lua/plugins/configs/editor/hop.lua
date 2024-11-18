a1d7a2cb0bf73f929c58d3d1e8075be0e11ed31b
Emmanuel Bustos Torres <ema2159@gmail.com>
2024-11-18 14:29:21 +0100

    Delete lua/plugins/configs/editor/hop.lua

--- lua/plugins/configs/editor/hop.lua
+++
@@ -1,7 +0,0 @@
-return function()
-  local hop = require("hop")
-  hop.setup()
-  vim.keymap.set({ "n", "v" }, "<leader>'", function()
-    hop.hint_char1()
-  end, { desc = "Hop to character" })
-end
